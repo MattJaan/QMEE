72caf274ff8db80889976d9b3f80a49a0f3ce134
MattJaan <jaansalum@gmail.com>
2026-01-16 13:50:49 -0500

    Assignment 2 files with an rds file that was made after ammending assignment1 r script

---
+++ Assignment2/Assignment2_1.R
@@ -0,0 +1,78 @@
+library(dplyr)
+library(tidyr)
+library(readr)
+library(ggplot2)
+
+# Reads RDS file into a data frame 
+df_avg <- read_rds("Slope_averages.rds")
+
+# Transposes the data frame but needs to made into a data frame again because 
+# transposing turns it into a matrix
+df_t <- as.data.frame(t(df_avg))
+
+# Stores all the number component of the well plate label, the label, 
+# and plate number into lists
+numbers <- as.numeric(rep(1:24,32))
+labels<- rep(rownames(df_t2),2)
+plate <- as.numeric(c(rep(1,384),rep(2,384)))
+
+# Makes a data frame called slope using the columns of the transposed data frame
+df_form <- data.frame(slope=c(df_t[,"plate1_avg"],df_t[,"plate2_avg"]))
+
+# Adds columns to the data frame using the lists above
+df_form$well<- labels
+df_form$number<- numbers
+df_form$plate<- plate
+
+# Using mutate to make a column "assay" when the value in the number column is
+# in one of the lists, each list results in corresponding assay types
+df_form2 <- df_form |> mutate(assay=case_when(
+  number %in% c(1,12,13,24)~"positive",
+  number %in% c(2,14)~"negative",
+  number %in% c(3,4,5,15,16,17)~"NADPH",
+  number %in% c(6,7,8,18,19,20)~"No protein",
+  number %in% c(9,10,11,21,22,23)~"Reductase"
+))
+
+# Using mutate to make a column called protein for when the number column value 
+# is in a list and plate column equals either 1 or 2
+df_fin <- df_form2 |> mutate(protein=case_when(
+  number %in% c(1:12) & plate ==1 ~"CYP 1A",
+  number %in% c(13:24) & plate ==1 ~"CYP 1B1",
+  number %in% c(1:12) & plate ==2 ~"CYP 1C1",
+  number %in% c(13:24) & plate ==2 ~"CYP 1D1"
+))
+
+# Printing to show the number of trails in each well plate, all the proteins,
+# assays and number of wells for each assay per protein
+print(df_fin |> count(plate, name="count"))
+print(df_fin |> count(protein, name="count"))
+print(df_fin |> count(assay, name="count"))
+print(df_fin |> count(plate, protein, assay, name="count"))
+
+# Class checking
+class(df_fin$slope)
+class(df_fin$well)
+class(df_fin$number)
+class(df_fin$plate)
+class(df_fin$assay)
+class(df_fin$protein)
+
+# Storing characters as factors
+df_fin$protein <- as.factor(df3$protein)
+df_fin$assay <- as.factor(df3$assay)
+df_fin$well <- as.factor(df3$well)
+
+summary(df_fin)
+
+# Saves data frame in easily readable RDS file
+saveRDS(df_fin, "Formatted_Data.rds")
+
+# Creates data frame grouping by protein to calculate the plate and total wells per protein
+df_fin_sum <- df_fin |> group_by(protein)|> summarise(avg_plate=mean(plate))
+
+# Makes a column graph for the plate # per protein
+ggplot(data=df_fin_sum, mapping=aes(x=protein,y= avg_plate)) + geom_col()
+       
+ 
+       
---
+++ Assignment2/Assignment2_2.R
@@ -0,0 +1,13 @@
+library(dplyr)
+library(tidyr)
+library(readr)
+library(ggplot2)
+
+# Reads in a data frame from the RDS file and stores it to df
+df <- read_rds("Formatted_Data.rds")
+
+# Makes a box and whisker plot for the slopes of each protein by assay type
+ggplot(data = df, mapping = aes(x = protein, y = slope, color = assay)) +
+  geom_boxplot()
+
+
