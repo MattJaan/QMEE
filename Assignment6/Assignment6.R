40d8b1b1b7029333696e03bb27ca5daa46942320
MattJaan <jaansalum@gmail.com>
2026-02-16 00:31:02 -0500

    Assignment 6 upload

---
+++ Assignment6/Assignment6.R
@@ -0,0 +1,39 @@
+library(ggplot2)
+library(readr)
+library(dplyr)
+library(tidyr)
+library(emmeans)
+library(effects)
+
+slopes <- read_rds("Formatted_Data.rds")
+
+slopes$assay<-as.character(slopes$assay) 
+
+#Sort the factors to better order
+slopes$assay <- factor(slopes$assay, levels = c("negative", "No protein", "Reductase","NADPH","positive"))
+
+
+lm_all<- lm(slope~assay*protein,data=slopes)
+
+plot(emmeans(lm_all, ~ assay*protein),comparison=TRUE)
+
+newdata <- expand.grid(assay = levels(slopes$assay),protein = levels(slopes$protein))
+
+datapred <- predict(lm_all, newdata, se.fit = TRUE)
+newdata$fit <- datapred$fit
+newdata$se <- datapred$se.fit
+newdata$lower <- newdata$fit - 1.96 * newdata$se
+newdata$upper <- newdata$fit + 1.96 * newdata$se
+
+ggplot() +
+  geom_point(data = slopes, aes(x=assay, y=slope, color = protein),
+             position = position_jitter(width = 0.1),
+             alpha = 0.6) +
+  
+  geom_line(data = newdata, aes(x=assay, y=fit, color = protein, group = protein)) +
+  
+  geom_ribbon(data = newdata,
+              aes(assay, ymin = lower, ymax = upper, fill = protein, group = protein),
+              alpha = 0.2) 
+  
+plot(lm_all)
